3b3b5ace45b868b91e25e74f47fd6c93f4e26cf0
Александр Дрожкин <Santay2009@yandex.ru>
2020-09-27 13:19:48 +0300

    Выпелен линий тип

--- lib/Extension/Extension.cs
+++ lib/Extension/Extension.cs
@@ -31,11 +31,10 @@ namespace Extension
         ///     Перебирает коллекцию IList и применяет к каждому элементу делегат ExtensionDelegateOutQ
         /// </summary>
         /// <typeparam name="T">TInput</typeparam>
-        /// <typeparam name="TQ">TOutput</typeparam>
         /// <param name="enumerable">Коллекция</param>
         /// <param name="action">Действие, которое нужно сделать с каждым элементом коллекции</param>
         /// <returns>Преобразованную коллекцию</returns>
-        public static IEnumerable<T> Foreach<T, TQ>(this IList<T> enumerable, Action<T> action)
+        public static IEnumerable<T> Foreach<T>(this IList<T> enumerable, Action<T> action)
         {
             for (int index = 0; index < enumerable.Count; index++)
                 action.Invoke(obj: enumerable[index: index]);
